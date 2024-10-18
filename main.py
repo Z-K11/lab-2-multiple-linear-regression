429bbc7d68219900b5cd2f19e7ac3b894f4c1311
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-10-18 21:15:48 +0500

    Verify the program reads data from the csv file

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 csv_data = pd.read_csv("FuelConsumptionCo2.csv")
+readable_data = csv_data[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+print(readable_data.head(9))
