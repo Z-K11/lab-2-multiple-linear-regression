625423c19fe13d85c1409c2751d34b8f80cf09d0
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-10-23 20:50:30 +0500

    Latest snapshot

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ import matplotlib.pyplot as plt
 csv_data = pd.read_csv("FuelConsumptionCo2.csv")
 readable_data = csv_data[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
 print(readable_data.head(9))
+plt.scatter(readable_data.ENGINESZE,readable_data.CO2EMISSIONS,color = 'blue')
