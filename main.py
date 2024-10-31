5df4ebd3f67784fb5b73c1c9bbc50bc67bbf0daa
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-10-31 23:59:08 +0500

    Fix typo add plot explanation

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ import matplotlib.pyplot as plt
 csv_data = pd.read_csv("FuelConsumptionCo2.csv")
 readable_data = csv_data[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
 print(readable_data.head(9))
-plt.scatter(readable_data.ENGINESZE,readable_data.CO2EMISSIONS,color = 'blue')
+plt.scatter(readable_data.ENGINESIZE,readable_data.CO2EMISSIONS,color = 'blue')
+# readable_data.ENGINESIZE selects the engine size column from the dara
+# plt.scatter(x-axix,y-axsis,chart plot color) the parameter passed for x-axis is x-axis value and the parameter passed for y-axis
+# is y-axis value it creates a scatter plot whih shows the relationship between engine size and co2Emissions
+
