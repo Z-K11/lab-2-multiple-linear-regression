b6b28d8636c12bde2cc275c5229beebc77aa1d7f
Z-K11 <zulqarnainbazidkhail@gmail.com>
2024-11-01 00:03:55 +0500

    Save a png file plotting relation between engine size and Co2 Emissions

--- main.py
+++ main.py
@@ -8,4 +8,10 @@ plt.scatter(readable_data.ENGINESIZE,readable_data.CO2EMISSIONS,color = 'blue')
 # readable_data.ENGINESIZE selects the engine size column from the dara
 # plt.scatter(x-axix,y-axsis,chart plot color) the parameter passed for x-axis is x-axis value and the parameter passed for y-axis
 # is y-axis value it creates a scatter plot whih shows the relationship between engine size and co2Emissions
+plt.xlabel("Engine Size")
+# labels the x-axis
+plt.ylabel("Co2 Emissions")
+# labels the y-axis
+plt.savefig("./pngFiles/engine-vs-co2.png")
+# Saves a png file of the scatter plot 
 
